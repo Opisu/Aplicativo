22746dfc398babe8ad004303f6c5065823e37bbb
Opisu <69693692+Opisu@users.noreply.github.com>
2020-08-14 15:44:32 -0300

    Add files via upload

---
+++ grupos_capas.js
@@ -0,0 +1,16 @@
+var grupos_capas = {
+				"opisu" : {title: "Capas Opisu", expanded:true},
+				"arba" : {title: "ARBA",  expanded: false},
+				"ign" : {title: "IGN",  expanded: false},
+				"geoinfra" : {title: "GeoInfra",  expanded: false},
+				"indec" : {title: "INDEC",  expanded: false},
+                "urbasig" : {title: "Urbasig", expanded: false},	
+		      	//"IdeHab" : {title: "IdeHab",  expanded: false},
+		      	
+           
+                "default": "Agregadas",
+                "consulta" : {title: "Resultados", expanded: false},
+                "background" : {title: "Capas Base", exclusive: true, expanded: true}
+				
+            };
+
